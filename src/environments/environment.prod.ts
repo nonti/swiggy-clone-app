6a97de16013227d122b64d238bb897b028ea55ae
Nonti Mbowane <nonty8@gmail.com>
2024-07-13 22:48:49 +0200

    added handling environment functionality

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+import { Environment } from "./environment";
+
+export const ProdEnvironment: Environment = {
+  db_uri: 'mongodb+srv://nontis:qsq4KcjMTTQgTI1q@swiggycloneapp.fdw8veb.mongodb.net/swiggyCloneApp?retryWrites=true&w=majority&appName=swiggyCloneApp'
+};
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
 import * as express from 'express';
+import * as mongoose from 'mongoose';
+import { getEnvironmentVariables } from './environments/environment';
 
 let app: express.Application = express();
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is Running on Port 3000');
 });
+
+mongoose.connect(getEnvironmentVariables().db_uri)
+  .then(() => {
+    console.log('Connected to MongoDB.');
+  })
+
+app.get('/api/login', (req, res) => {
+  const data = [{ name: 'Rhandzu' }];
+  res.status(200).send(data);
+});
+
+app.get('/api/user/test', (req, res) => {
+  console.log('test');
+  res.send('test');
+})
+
+app.use((req, res, next) => {
+  console.log('middleware');
+  next();
+})
