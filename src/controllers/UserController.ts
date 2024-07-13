b75e818f675392ed82bce8b304740ab5665e6e3b
Nonti Mbowane <nonty8@gmail.com>
2024-07-14 00:29:33 +0200

    added a model file for mongodb schema

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,3 +1,4 @@
+import User from "../models/User";
 
 export class UserContoller {
 
@@ -7,7 +8,21 @@ export class UserContoller {
     // (req as any).errorStatus = 422;
     // const error = new Error('User Email or Password does not match');
     // next(error);
-    res.send(req.body);
+    // res.send(req.body);
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = new User({
+      email,
+      password
+    });
+
+    user.save().then((user) => {
+      res.send(user);
+    }).catch((err) => {
+      next(err);
+    });
   }
 
   static test(req, res, next) {
